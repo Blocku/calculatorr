c9a360efeed92cb1b625fed1efb00a597ccb74ae
Blocku b <urent111@yandex.ru>
2024-02-02 15:47:02 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        water: '#437DD4',
+        lightWater: '#6A94D4'
+      },
+    },
+    
+  },
+  plugins: [],
+}
+
