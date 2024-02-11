9479b5c127c7c3cdc6803cfc8253d81742c2acd9
Blocku b <urent111@yandex.ru>
2024-02-11 16:26:24 +0000

    new script and a little change interface

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: ["./index.html"],
   theme: {
     extend: {
       fontFamily: {
-        rubik: ["Rubik", "sans-serif", ...defaultTheme.fontFamily.sans],
+        rubik: ["Rubik", "sans-serif"]
       },
     },
   },
